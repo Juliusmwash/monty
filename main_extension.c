fb61bad716ea0c098ec19446d1a5a377a4ba96f5
Juliusmwash <cutejuliusmwash@gmail.com>
2023-03-26 12:48:58 +0300

    Arrange files

--- main_extension.c
+++ main_extension.c
@@ -16,3 +16,28 @@ void f(instruction_t **p, stack_tt **sk, FILE *t, unsigned int *l)
 	fclose(t);
 	unknown_instruction_error(p, *l);
 }
+
+/**
+ * status_check - checks for stack or queue mode
+ * @op: linked list pointer to opcode
+ * @status: mode switching variable
+ * Return: nothing
+ */
+
+int status_check(instruction_t **op, int *status)
+{
+	if ((*op)->opcode != NULL)
+	{
+		if (strcmp((*op)->opcode, "queue") == 0)
+		{
+			*status = 1;
+			return (1);
+		}
+		else if (strcmp((*op)->opcode, "stack") == 0)
+		{
+			*status = 0;
+			return (1);
+		}
+	}
+	return (0);
+}
